b38c2dd2b860e3cddbba59bfdde029e72e6a5641
Dimas Awang Kusuma <arjunane.kajen@gmail.com>
2020-11-05 17:01:43 +0700

    update page.dart

---
+++ system/page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Controller<T extends StatefulWidget> extends State<T> {
+  void refresh(Function refresh) {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      refresh();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    throw UnimplementedError();
+  }
+}
+
+abstract class View<T extends Controller> extends StatelessWidget {
+  final T prop;
+  View(this.prop);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    throw UnimplementedError();
+  }
+}
